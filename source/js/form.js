d844d659d608b9172092b0288dd62df158ba3159
Alina Glebova <alina.lina2407@gmail.com>
2021-01-22 18:45:32 +0300

    Исправлены ошибки по требованиям к проекту

--- source/js/form.js
+++ source/js/form.js
@@ -1,24 +1,25 @@
-let sendButton = document.querySelector('.form__button');
-let searchForm = document.querySelector('.form');
+'use strict';
+
+var searchForm = document.querySelector('.form');
 
 searchForm.addEventListener('submit', function (evt) {
-  let isValidateSuccess = true;
+  var isValidateSuccess = true;
 
   // получаем поля формы
-  let name = document.getElementById('name');
-  let tel = document.getElementById('tel');
+  var name = document.getElementById('name');
+  var tel = document.getElementById('tel');
 
-  if (name.value === ''){
+  if (name.value === '') {
     name.required = true;
     isValidateSuccess = false;
   }
 
-  if (tel.value === ''){
+  if (tel.value === '') {
     tel.required = true;
     isValidateSuccess = false;
   }
 
-  if(isValidateSuccess !== true){
+  if (isValidateSuccess !== true) {
     evt.preventDefault();
   }
 });
